6a8d83eebeaada04d330c4ff891f59b40a0b05d8
Longdznood <l3231799@gmail.com>
2025-07-16 18:26:01 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,15 @@
 #!/bin/sh
 cd server
+
+# Tải Geyser nếu chưa có
+if [ ! -f "plugins/Geyser-Spigot.jar" ]; then
+  echo "Đang tải GeyserMC..."
+  mkdir -p plugins
+  curl -L -o plugins/Geyser-Spigot.jar https://download.geysermc.org/builds/Geyser-Spigot/849/download
+fi
+
+# Chấp nhận EULA
 echo "eula=true" > eula.txt
+
+# Chạy server
 java -Xms1G -Xmx1G -jar paperclip.jar nogui
